40f94683ec541e5f28801940fefeb63ea9d8220b
Joshua Maymir <joshua.maymir@saschools.org>
2021-04-11 22:16:06 -0400

    Added argparser functionality to program

--- src/pdf_mailmerge/produce_pdfs.py
+++ src/pdf_mailmerge/produce_pdfs.py
@@ -11,18 +11,25 @@ import sys
 import pdfkit
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='produce pdfs of html files in target directory')
 parser.add_argument('directory',
                     help= 'directory of html files converted into pdfs',
                     type = str)
-
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q', '--quiet',
+                    help = 'do not display list progress',
+                    action = 'store_true')
+group.add_argument('-v', '--verbose',
+                    help = 'display list progress',
+                    action = 'store_true',
+                    default = True)
 args = parser.parse_args()
 
-dir = vars(args)['directory']
-
-def pdf(dir):
+def pdf(args):
 
     renders = []
+
+    dir = args.directory
 
     for root, dirs, files in os.walk(dir):
         if not dirs:
@@ -30,28 +37,50 @@ def pdf(dir):
                 renders.append({"directory": root,
                                 "filename": file})
 
-    for render in tqdm(renders):
+    if args.quiet:
+        for render in renders:
+            # Windows uses back slashes
+            # Use os.path.join Join method
+            in_path_filename = os.path.join(render["directory"], render["filename"])
 
-        # Windows uses back slashes
-        # Use os.path.join Join method
-        in_path_filename = os.path.join(render["directory"], render["filename"])
+            out_path = os.path.join("pdfs", render["directory"].replace(dir, ""))
+            out_path_filename = os.path.join(out_path, render["filename"].replace('html', 'pdf'))
 
-        out_path = os.path.join("pdfs", render["directory"].replace(dir, ""))
-        out_path_filename = os.path.join(out_path, render["filename"].replace('html', 'pdf'))
+            # create school dir if not exists
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
 
-        # create school dir if not exists
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
+            # only create PDFs that don't exist
+            if not os.path.exists(out_path_filename):
+                # create pdf
+                pdfkit.from_file(in_path_filename,
+                                 out_path_filename,
+                                 options={'quiet': '',
+                                          'page-size': 'A4',
+                                          'dpi': 650})
 
-        # only create PDFs that don't exist
-        if not os.path.exists(out_path_filename):
-            # create pdf
-            pdfkit.from_file(in_path_filename,
-                             out_path_filename,
-                             options={'quiet': '',
-                                      'page-size': 'A4',
-                                      'dpi': 650})
+    if args.verbose:
+        for render in tqdm(renders):
 
+            # Windows uses back slashes
+            # Use os.path.join Join method
+            in_path_filename = os.path.join(render["directory"], render["filename"])
+
+            out_path = os.path.join("pdfs", render["directory"].replace(dir, ""))
+            out_path_filename = os.path.join(out_path, render["filename"].replace('html', 'pdf'))
+
+            # create school dir if not exists
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+
+            # only create PDFs that don't exist
+            if not os.path.exists(out_path_filename):
+                # create pdf
+                pdfkit.from_file(in_path_filename,
+                                 out_path_filename,
+                                 options={'quiet': '',
+                                          'page-size': 'A4',
+                                          'dpi': 650})
 
 if __name__ == "__main__":
-     pdf(dir)
+     pdf(args)
